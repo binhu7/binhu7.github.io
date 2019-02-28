1da4af3bd8673a4f7fc27463476bc400812edf65
binhu7 <41927034+binhu7@users.noreply.github.com>
2019-02-28 11:52:42 -0600

    Add files via upload

---
+++ courses/ECE598/files/SDPEx.m
@@ -0,0 +1,13 @@
+clear;
+% set A matrix, change this based on your need
+A=eye(3)*0.9;
+%implement the LMI
+cvx_begin sdp quiet
+    variable P(3,3) semidefinite;     
+     minimize(0)
+    subject to
+    A'*P*A-P<=-0.001*eye(3); 
+ P>=0.001*eye(3)
+cvx_end
+
+P
